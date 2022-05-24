ed3d3ca821a8c287f0ffa7daeba5be0d13e58608
Casa\Miguel <miguepm18@gmail.com>
2022-05-24 20:58:59 +0200

    Generado el formulario de registro con estilos aplicados. Tambien se ha cambiado el metodo de inicio de sesión

--- icommerceApp/src/app/registro-cliente-page/registro-cliente-page.page.ts
+++ icommerceApp/src/app/registro-cliente-page/registro-cliente-page.page.ts
@@ -9,25 +9,57 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 export class RegistroClientePagePage implements OnInit {
 
   validations_form: FormGroup;
+  nombreApellidos:boolean;
+  usuario:boolean;
+  passwords:boolean;
+  direccionEmail:boolean;
+  dniFechaNacimiento:boolean;
+  allCorrect:boolean;
+  mensaje:string;
   constructor(public formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    //BOLEANOS PARA CONTROLAR QUE CAMPO SE MUESTRA
+    this.nombreApellidos = true;
+    this.usuario = false;
+    this.passwords = false;
+    this.direccionEmail = false;
+    this.dniFechaNacimiento = false;
+    this.allCorrect = false;
+
+    //MENSAJE DE INFORMACIÓN PARA EL USUARIO
+    this.mensaje = "Introduzca su nombre y apellidos";
+
+    //FORMULARIO
     this.validations_form = this.formBuilder.group({
       nombre: new FormControl('', Validators.compose([
-      Validators.pattern('^[a-zA-Z0-9]+$'),
       Validators.required
       ])),
-      usuario: new FormControl('', Validators.required),
-      apellidos: new FormControl('', Validators.required),
-      contraseña: new FormControl('', Validators.required),
-      confirmaContraseña: new FormControl('', Validators.compose([
-      Validators.required,
-      Validators.pattern('^[a-zA-Z0-9_.+-]+[@]{1}[a-zA-Z0-9-]+[.]{1}[a-zA-Z]+$')
+      usuario: new FormControl('', Validators.compose([
+        Validators.required
       ])),
-      direccion: new FormControl('', Validators.required),
-      email: new FormControl('', Validators.pattern('true')),
-      dni: new FormControl('', Validators.required),
-      fechaNacimiento: new FormControl('', Validators.required)
+      apellidos: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      contraseña: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      confirmaContraseña: new FormControl('', Validators.compose([
+      Validators.required
+      ])),
+      direccion: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')
+      ])),
+      dni: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      fechaNacimiento: new FormControl('', Validators.compose([
+        Validators.required
+      ]))
       });
   }
 
@@ -40,6 +72,39 @@ export class RegistroClientePagePage implements OnInit {
     }
     };
     this.navCtrl.navigateForward('/user', navigationExtras);*/
+  }
+
+  //METODO QUE COMPRUEBA Y CAMBIA QUE CAMPOS SE MUESTRAN
+  siguiente(){
+
+    if(this.dniFechaNacimiento && (!this.usuario && !this.nombreApellidos && !this.nombreApellidos && !this.direccionEmail)){ //REQUISITOS PARA QUE PASE A LA PANTALLA DE INFORMACION GENERAL DEL FORMULARIO
+      this.dniFechaNacimiento = false;
+      this.allCorrect = true;
+      this.mensaje = "Informacion general";
+    }
+    if(this.direccionEmail && (!this.usuario && !this.nombreApellidos && !this.nombreApellidos && !this.dniFechaNacimiento)){//REQUISITOS PARA QUE PASE A LA PANTALLA DE DNI/FECHA DE NACIMIENTO
+      this.direccionEmail = false;
+      this.dniFechaNacimiento = true;
+      this.mensaje = "Introduce tu DNI y Fecha de nacimiento";
+      
+    }
+    if(this.passwords && (!this.usuario && !this.nombreApellidos && !this.direccionEmail && !this.dniFechaNacimiento)){//REQUISITOS PARA QUE PASE A LA PANTALLA DE DIRECCION/EMAIL
+      this.passwords = false;
+      this.direccionEmail = true;
+      this.mensaje = "Introduce tu dirección y correo";
+      
+    }
+    if(this.usuario && (!this.nombreApellidos && !this.passwords && !this.direccionEmail && !this.dniFechaNacimiento)){//REQUISITOS PARA QUE PASE A LA PANTALLA DE CONTRASEÑAS
+      this.usuario = false;
+      this.passwords = true;
+      this.mensaje = "Introduce tu contraseña";
+      
+    }
+    if(this.nombreApellidos && (!this.usuario && !this.passwords && !this.direccionEmail && !this.dniFechaNacimiento)){ //REQUISITOS PARA QUE PASE A LA PANTALLA DE USUARIO
+      this.usuario = true;
+      this.nombreApellidos = false;
+      this.mensaje = "Introduce tu nombre de usuario";
+    }
   }
 
 }
